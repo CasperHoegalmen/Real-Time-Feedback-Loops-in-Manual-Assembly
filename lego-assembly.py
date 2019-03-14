5cc258c42418781e093804526919c6df61b09c1d
MohanadZ <mzeito14@student.aau.dk>
2019-03-14 22:30:31 +0100

    Second iteration
    The calculation is still not quite right :(

--- lego-assembly.py
+++ lego-assembly.py
@@ -4,6 +4,10 @@ import cv2
 import math
 
 cap = cv2.VideoCapture(0)
+hue = 0
+B = 0
+G = 0
+R = 0
 
 def cameraFrame():
     while(cap.isOpened()):
@@ -20,25 +24,45 @@ def cameraFrame():
 
     
 def RGB2HSI(frame):
-    hue = 0
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #cv2.imshow('hsv', hsv)
 
     #Iterate over each pixel in the image
-    for x in range(0, frame.shape[0]):
-        for y in range(0, frame.shape[1]):
+    for x in range(0, frame.shape[1]):
+        for y in range(0, frame.shape[0]):
             #Split the B, G, R channels (it is BGR and not RGB because read() function in openCV returns it in that format)
             B = frame[y, x, 0]
             G = frame[y, x, 1]
             R = frame[y, x, 2]
 
-            #print("This is ", frame.shape)
-            print("B IS...................", B, " and G is ", G)
-
-            #Calculating intensity
-            intensity = (B + G + R) / 3
+            #print("B is ", B)
+            print("B is ", B, "\nG is ", G, "\nR is ", R)
 
             #Calculating Saturation
             minimum_value = min(B, min(G, R))
             saturation = 1 - 3 * (minimum_value) / (R + G + B)
+
+            #Calculating intensity
+            intensity = (B + G + R) / 3
+
+            #Calculating Hue 
+            if saturation != 0:
+                hueCalculation = 0.5 * ((R - G) + (R - B)) / math.sqrt(math.pow((R - G), 2) + (R - B) * (G - B))
+
+                # if H < -1:
+                #     H = -1
+                # elif H > 1:
+                #     H = 1
+
+                def hueRange(hueCalculation):
+                    return min(1, max(hueCalculation, -1))
+                H = hueRange(hueCalculation)
+
+                if B <= G:        
+                    hue = math.acos(H)
+                elif B > G:
+                    hue = 2 * math.pi - math.acos(H)
 
             #This is to ensure that we do not divide by 0 when calculating saturation
             if R + G + B == 0:
@@ -46,29 +70,11 @@ def RGB2HSI(frame):
                 intensity = 0
                 hue = 0
 
-            #Calculating Hue 
-            if saturation != 0:
-                H = 0.5 * ((R - G) + (R - B)) / math.sqrt(math.pow((R - G), 2) + (R - B) * (G - B))
-
-                if B <= G:        
-                    hue = math.acos(H)
-                elif B > G:
-                    hue = 2 * math.pi - math.acos(H)
+            print("Hue is ", hue, "\nSaturation is ", saturation, "\nIntensity is ", intensity)
 
             frame[y, x, 0] = hue * 255 / (2 * math.pi)
             frame[y, x, 1] = saturation * 255
             frame[y, x, 2] = intensity
-
-            #G, R = cv2.split(frame)
-            #if np.less_equal(B, G):
-
-            #Calculate Hue
-            #if B <= G:
-            #    hue = math.acos(0.5 * ((R - G) + (R - B)) / math.sqrt(math.pow((R - G), 2) + (R - B) * (G - B)))
-            #    print("Hue for ", x, " is ", hue)
-            #elif B > G:
-            #    hue = 2 * math.pi - math.acos(0.5 * ((R - G) + (R - B)) / math.sqrt(math.pow((R - G), 2) + (R - B) * (G - B)))
-    
 
 cameraFrame()
 # When everything done, release the capture
