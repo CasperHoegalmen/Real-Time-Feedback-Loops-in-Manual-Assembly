07c72c10512c8e08572cee67c1230e7642803a22
GunleifGunne <andis8722@hotmail.com>
2019-04-17 16:34:59 +0200

    Blah

---
+++ Feedback Loop Visualization/Assets/Scripts/BrickChanger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickChanger : MonoBehaviour
+{
+    public GameObject[] bricks;
+    public Material[] materials;
+
+    private Renderer[] childMaterials;
+
+    Material opaque, transparent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        opaque = materials[0];
+        transparent = materials[1];
+
+        ChangeMaterial(0, "opaque");
+    }
+
+    public void ChangeMaterial(int brickToChange, string material)
+    {
+        if(material == "transparent")
+        {
+            childMaterials = bricks[brickToChange].GetComponentsInChildren<MeshRenderer>();
+            foreach (Renderer rend in childMaterials)
+            {
+                rend.material = transparent;
+            }
+        }
+        else if(material == "opaque")
+        {
+            childMaterials = bricks[brickToChange].GetComponentsInChildren<MeshRenderer>();
+            foreach (Renderer rend in childMaterials)
+            {
+                rend.material = opaque;
+            }
+        }
+
+    }
+}
---
+++ Feedback Loop Visualization/Assets/Scripts/ImageChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageChanger : MonoBehaviour
+{
+    public Image[] images;
+    public Sprite[] imageSprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeImage(int imageIndex, int spriteIndex)
+    {
+        images[imageIndex].sprite = imageSprites[spriteIndex];
+    }
+}
