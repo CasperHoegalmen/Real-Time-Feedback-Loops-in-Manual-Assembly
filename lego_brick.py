12279f033261e49b9261ebb25bc2e9ac50dd3d94
metigel94 <leon-mueller@hotmail.de>
2019-05-03 14:49:11 +0200

    Added complex model varibles in LegoBrick class

--- lego_brick.py
+++ lego_brick.py
@@ -106,4 +106,131 @@ brick10.min_area = 1920 - LegoBrick.area_range
 brick10.max_area = 1920 + LegoBrick.area_range
 brick10.color = "Red"
 
-lego_model = [brick1, brick2, brick3, brick4, brick5, brick6, brick7, brick8, brick9, brick10]
+brick11 = LegoBrick()
+brick11.x = 348 - LegoBrick.position_offset
+brick11.y = 242 - LegoBrick.position_offset
+brick11.h = 287 + LegoBrick.position_offset
+brick11.w = 393 + LegoBrick.position_offset
+brick11.min_area = 1920 - LegoBrick.area_range
+brick11.max_area = 1920 + LegoBrick.area_range
+brick11.color = "Red"
+
+brick12 = LegoBrick()
+brick12.x = 348 - LegoBrick.position_offset
+brick12.y = 242 - LegoBrick.position_offset
+brick12.h = 287 + LegoBrick.position_offset
+brick12.w = 393 + LegoBrick.position_offset
+brick12.min_area = 1920 - LegoBrick.area_range
+brick12.max_area = 1920 + LegoBrick.area_range
+brick12.color = "Yellow"
+
+brick13 = LegoBrick()
+brick13.x = 348 - LegoBrick.position_offset
+brick13.y = 242 - LegoBrick.position_offset
+brick13.h = 287 + LegoBrick.position_offset
+brick13.w = 393 + LegoBrick.position_offset
+brick13.min_area = 1920 - LegoBrick.area_range
+brick13.max_area = 1920 + LegoBrick.area_range
+brick13.color = "Purple"
+
+brick14 = LegoBrick()
+brick14.x = 348 - LegoBrick.position_offset
+brick14.y = 242 - LegoBrick.position_offset
+brick14.h = 287 + LegoBrick.position_offset
+brick14.w = 393 + LegoBrick.position_offset
+brick14.min_area = 1920 - LegoBrick.area_range
+brick14.max_area = 1920 + LegoBrick.area_range
+brick14.color = "Red"
+
+brick15 = LegoBrick()
+brick15.x = 348 - LegoBrick.position_offset
+brick15.y = 242 - LegoBrick.position_offset
+brick15.h = 287 + LegoBrick.position_offset
+brick15.w = 393 + LegoBrick.position_offset
+brick15.min_area = 1920 - LegoBrick.area_range
+brick15.max_area = 1920 + LegoBrick.area_range
+brick15.color = "Green"
+
+brick16 = LegoBrick()
+brick16.x = 348 - LegoBrick.position_offset
+brick16.y = 242 - LegoBrick.position_offset
+brick16.h = 287 + LegoBrick.position_offset
+brick16.w = 393 + LegoBrick.position_offset
+brick16.min_area = 1920 - LegoBrick.area_range
+brick16.max_area = 1920 + LegoBrick.area_range
+brick16.color = "Blue"
+
+brick17 = LegoBrick()
+brick17.x = 348 - LegoBrick.position_offset
+brick17.y = 242 - LegoBrick.position_offset
+brick17.h = 287 + LegoBrick.position_offset
+brick17.w = 393 + LegoBrick.position_offset
+brick17.min_area = 1920 - LegoBrick.area_range
+brick17.max_area = 1920 + LegoBrick.area_range
+brick17.color = "Blue"
+
+brick18 = LegoBrick()
+brick18.x = 348 - LegoBrick.position_offset
+brick18.y = 242 - LegoBrick.position_offset
+brick18.h = 287 + LegoBrick.position_offset
+brick18.w = 393 + LegoBrick.position_offset
+brick18.min_area = 1920 - LegoBrick.area_range
+brick18.max_area = 1920 + LegoBrick.area_range
+brick18.color = "Purple"
+
+brick19 = LegoBrick()
+brick19.x = 348 - LegoBrick.position_offset
+brick19.y = 242 - LegoBrick.position_offset
+brick19.h = 287 + LegoBrick.position_offset
+brick19.w = 393 + LegoBrick.position_offset
+brick19.min_area = 1920 - LegoBrick.area_range
+brick19.max_area = 1920 + LegoBrick.area_range
+brick19.color = "Yellow"
+
+brick20 = LegoBrick()
+brick20.x = 348 - LegoBrick.position_offset
+brick20.y = 242 - LegoBrick.position_offset
+brick20.h = 287 + LegoBrick.position_offset
+brick20.w = 393 + LegoBrick.position_offset
+brick20.min_area = 1920 - LegoBrick.area_range
+brick20.max_area = 1920 + LegoBrick.area_range
+brick20.color = "Green"
+
+brick21 = LegoBrick()
+brick21.x = 348 - LegoBrick.position_offset
+brick21.y = 242 - LegoBrick.position_offset
+brick21.h = 287 + LegoBrick.position_offset
+brick21.w = 393 + LegoBrick.position_offset
+brick21.min_area = 1920 - LegoBrick.area_range
+brick21.max_area = 1920 + LegoBrick.area_range
+brick21.color = "Red"
+
+brick22 = LegoBrick()
+brick22.x = 348 - LegoBrick.position_offset
+brick22.y = 242 - LegoBrick.position_offset
+brick22.h = 287 + LegoBrick.position_offset
+brick22.w = 393 + LegoBrick.position_offset
+brick22.min_area = 1920 - LegoBrick.area_range
+brick22.max_area = 1920 + LegoBrick.area_range
+brick22.color = "Green"
+
+brick23 = LegoBrick()
+brick23.x = 348 - LegoBrick.position_offset
+brick23.y = 242 - LegoBrick.position_offset
+brick23.h = 287 + LegoBrick.position_offset
+brick23.w = 393 + LegoBrick.position_offset
+brick23.min_area = 1920 - LegoBrick.area_range
+brick23.max_area = 1920 + LegoBrick.area_range
+brick23.color = "Green"
+
+brick24 = LegoBrick()
+brick24.x = 348 - LegoBrick.position_offset
+brick24.y = 242 - LegoBrick.position_offset
+brick24.h = 287 + LegoBrick.position_offset
+brick24.w = 393 + LegoBrick.position_offset
+brick24.min_area = 1920 - LegoBrick.area_range
+brick24.max_area = 1920 + LegoBrick.area_range
+brick24.color = "Yellow"
+
+lego_model = [brick1, brick2, brick3, brick4, brick5, brick6, brick7, brick8, brick9, brick10, 
+                brick11, brick12, brick13, brick14, brick15, brick16, brick17, brick18, brick19, brick20, brick21, brick22, brick23, brick24]
